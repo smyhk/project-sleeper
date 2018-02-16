07d24fef2ef669409cbcc122433b55479ec7a63c
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-16 13:55:53 -0800

    Define game window frame attributes

--- src/com/smyhktech/sleeper/GameMain.java
+++ src/com/smyhktech/sleeper/GameMain.java
@@ -1,13 +1,29 @@
 package com.smyhktech.sleeper;
 
-public class GameMain implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameMain extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;			   // Width in pixels
 	public static int height = width / 16 * 9;  // Aspect ratio
 	public static int scale = 3;				   // Scalar for windowing
 	
 	private Thread gameThread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	// Constructor
+	public GameMain() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 	
 	public synchronized void startGame() {
 		running = true;
@@ -25,9 +41,22 @@ public class GameMain implements Runnable {
 		}
 	}
 
-	
 	public void run() {
 		while (running) {
+			System.out.println("Running!!!");
 		}
+	}
+	
+	public static void main(String[] args) {
+		GameMain game = new GameMain();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Sleeper");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.startGame();
 	}
 }
