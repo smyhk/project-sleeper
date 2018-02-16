cb3fb2d3e5a716230b98780a1158bfdf697672cd
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-16 13:22:49 -0800

    Start and stop game thread

--- src/com/smyhktech/sleeper/GameMain.java
+++ src/com/smyhktech/sleeper/GameMain.java
@@ -1,8 +1,24 @@
 package com.smyhktech.sleeper;
 
-public class GameMain {
+public class GameMain implements Runnable {
 	
 	public static int width = 300;			   // Width in pixels
 	public static int height = width / 16 * 9;  // Aspect ratio
 	public static int scale = 3;				   // Scalar for windowing
+	
+	private Thread gameThread;
+	
+	public synchronized void startGame() {
+		gameThread = new Thread(this, "Game");
+		gameThread.start();
+	}
+	
+	public synchronized void stopGame() {
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
