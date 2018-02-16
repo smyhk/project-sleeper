337e4c9b39b975ec427bf3967d4905c86ce177b4
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-16 13:02:02 -0800

    Initial commit and define screen resolution

---
+++ src/com/smyhktech/sleeper/GameMain.java
@@ -0,0 +1,8 @@
+package com.smyhktech.sleeper;
+
+public class GameMain {
+	
+	public static int width = 300;			   // Width in pixels
+	public static int height = width / 16 * 9;  // Aspect ratio
+	public static int scale = 3;				   // Scalar for windowing
+}
