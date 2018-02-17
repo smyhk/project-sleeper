de293b1c8542f43da69ae209ba7f866b8c95992f
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-16 16:22:39 -0800

    Define buffered image and pixel container

--- src/com/smyhktech/sleeper/GameMain.java
+++ src/com/smyhktech/sleeper/GameMain.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -19,6 +21,9 @@ public class GameMain extends Canvas implements Runnable {
 	private Thread gameThread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // Placeholder for drawing images
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();		  // Draws on the placeholder
 	
 	// Constructor
 	public GameMain() {
