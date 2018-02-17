61463d8542e7f1f37d54a9f718f7a3f5090ca734
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-16 18:46:41 -0800

    Prevent out of bounds on pixel array

--- src/com/smyhktech/sleeper/GameMain.java
+++ src/com/smyhktech/sleeper/GameMain.java
@@ -1,7 +1,6 @@
 package com.smyhktech.sleeper;
 
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
--- src/com/smyhktech/sleeper/graphics/Screen.java
+++ src/com/smyhktech/sleeper/graphics/Screen.java
@@ -4,6 +4,9 @@ public class Screen {
 
 	private int width, height;
 	public int[] pixels;
+	
+	int xtime = 0, ytime = 50;
+	int counter = 0;
 
 	public Screen(int width, int height) {
 		this.width = width;
@@ -25,9 +28,15 @@ public class Screen {
 	 * Draws on the screen
 	 */
 	public void render() {
+		counter++;
+		if (counter % 10 == 0) xtime++;
+		if (counter % 80 == 0) ytime++;
+		
 		for (int y = 0; y < height; y++) {
+			if (ytime >= height) break;
 			for (int x = 0; x < width; x++) {
-				pixels[40 + 100 * width] = 0xff00ff;
+				if (xtime >= width) break;
+				pixels[xtime + ytime * width] = 0xff00ff;
 			}
 		}
 	}
