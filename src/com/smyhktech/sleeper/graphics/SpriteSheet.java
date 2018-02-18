61a1cd80215453b4de208d9a82f3d1cd9ceefacd
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-17 18:28:39 -0800

    Start sprite sheets

---
+++ src/com/smyhktech/sleeper/graphics/SpriteSheet.java
@@ -0,0 +1,35 @@
+/**
+ * Manages sprite sheets and caching to memory
+ */
+package com.smyhktech.sleeper.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+	private String path;
+	private int size = 0;
+	public int[] pixels;
+	
+	// Constructor
+	public SpriteSheet(String path, int size) {
+		this.path = path;
+		this.size = size;
+		pixels = new int[size * size];
+		loadSheet();
+	}
+	
+	private void loadSheet() {
+		try {
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0,  0, w, h, pixels, 0, w);  // Loads the sprite sheet into the pixels array
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
