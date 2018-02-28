48e16e07329c8d743def67f21d5d5ac7cb534393
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-02-27 16:24:02 -0800

    Remove unneeded to type declaration

--- src/com/smyhktech/sleeper/entity/spawner/Spawner.java
+++ src/com/smyhktech/sleeper/entity/spawner/Spawner.java
@@ -5,15 +5,15 @@ import com.smyhktech.sleeper.level.Level;
 
 public class Spawner extends Entity {
 	
-	public enum Type {
+	protected enum Type {
 		MOB, PARTICLE;
 	}
 	
-	private Type type;
+	//private Type type;
 	
 	public Spawner(int x, int y, Type type, int amount, Level level) {
 		this.x = x;
 		this.y = y;
-		this.type = type;
+		//this.type = type;
 	}
 }
